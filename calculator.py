7670f4ae0f1fe630ae4d558da5cac07a89ee277f
ilkinm <ilkma1998@gmail.com>
2024-08-16 03:32:22 +0000

    Add initial calculator interface

---
+++ calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def add(self, a, b):
+        pass
+
+    def subtract(self, a, b):
+        pass
+
+    def divide(self, a, b):
+        pass
+
+    def multiply(self, a, b):
+        pass
+
+def main():
+    calc = Calculator()
+    # Add user interface code here
+
+if __name__ == "__main__":
+    main()
